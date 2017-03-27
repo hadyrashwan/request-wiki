55be7ccd6740446ecf958ab872653c4f20feda88
hadyRashwan <h2rashwan@gmail.com>
2017-03-27 13:49:50 +0200

    try with port 5000

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/', function(req, res) {
     res.send('Hello World!'+"</br>" + JSON.stringify(wikiResponse))
 })
 
-app.listen(8000, function() {
+app.listen(5000, function() {
     console.log(' app listening on port 3000! ')
 })
